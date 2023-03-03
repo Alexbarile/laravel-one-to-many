869fd09cabd19ea6ba9d7926483ae9bcc0ea8d3e
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-03 16:51:01 +0100

    create funzioni in Model Type e Model Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,13 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 // STR
 use Illuminate\Support\Str;
+// richiamo MODEL TYPE
+use App\Models\Type;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title', 'content', 'slug', 'type_id'];
 
     public static function generateSlug($title){
         return Str::slug($title, '-');
+    }
+
+    public function type(){
+        return $this->belongsTo(Type::class);
     }
 }
